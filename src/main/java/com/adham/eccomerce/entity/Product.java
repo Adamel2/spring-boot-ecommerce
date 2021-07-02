9b7cdceea8882d246d9ad2c9d895180f879dba8d
Adam ELdda <elddawork@gmail.com>
2021-07-02 16:16:14 +0300

    git push test

--- src/main/java/com/adham/eccomerce/entity/Product.java
+++ src/main/java/com/adham/eccomerce/entity/Product.java
@@ -8,12 +8,20 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
+
+/**
+ * 
+ * @author adam eldda
+ *
+ */
 
 @Entity
 @Table(name = "product")
@@ -24,6 +32,10 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name = "category_id", nullable = false)
+	private ProductCategory category;
 	
 	@Column(name = "sku")
 	private String sku;
